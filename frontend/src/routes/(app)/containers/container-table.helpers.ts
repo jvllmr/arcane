95f2f168b5dde3d797408cf55f4d1f6598a998da
Kyle Mendell <ksm@ofkm.us>
2026-07-08 17:10:38 -0500

    chore: group codemirror deps

--- frontend/src/routes/(app)/containers/container-table.helpers.ts
+++ frontend/src/routes/(app)/containers/container-table.helpers.ts
@@ -62,13 +62,21 @@ export function groupContainerByProject(container: ContainerSummaryDto): string 
 
 export function getContainerStatusLabel(state: string): string {
 	switch (state) {
-		case 'created': return m.containers_status_created();
-		case 'restarting': return m.containers_status_restarting();
-		case 'running': return m.containers_status_running();
-		case 'removing': return m.containers_status_removing();
-		case 'paused': return m.containers_status_paused();
-		case 'exited': return m.containers_status_exited();
-		case 'dead': return m.containers_status_dead();
-		default: return state ? state.charAt(0).toUpperCase() + state.slice(1) : m.common_unknown();
+		case 'created':
+			return m.containers_status_created();
+		case 'restarting':
+			return m.containers_status_restarting();
+		case 'running':
+			return m.containers_status_running();
+		case 'removing':
+			return m.containers_status_removing();
+		case 'paused':
+			return m.containers_status_paused();
+		case 'exited':
+			return m.containers_status_exited();
+		case 'dead':
+			return m.containers_status_dead();
+		default:
+			return state ? state.charAt(0).toUpperCase() + state.slice(1) : m.common_unknown();
 	}
 }
